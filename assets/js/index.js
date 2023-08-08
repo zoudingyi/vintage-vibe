8ca1e606538778cefc0597250ec3067dba919632
zoudingyi <zoudingyi@qq.com>
2023-08-08 17:13:20 +0800

    :sparkles: feat(custom): add pictures.

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,104 @@ let renderPool = []; // 渲染池
 
 const mainContainer = document.querySelector(".main-container");
 
-// 创建弹窗
+// err弹窗html
+const errHtml = `
+  <div class="aesthetic-windows-95-modal">
+    <div class="aesthetic-windows-95-modal-title-bar">
+      <div class="aesthetic-windows-95-modal-title-bar-text">
+        Error
+      </div>
+
+      <div class="aesthetic-windows-95-modal-title-bar-controls">
+        <div class="aesthetic-windows-95-button-title-bar">
+          <button>×</button>
+        </div>
+      </div>
+    </div>
+    <!-- Content -->
+    <div class="aesthetic-windows-95-modal-content">
+      <div class="aesthetic-effect-text-glitch" data-glitch="Back to 90s ❤" style="margin: 7px auto">
+        Back to 90s ❤
+      </div>
+
+      <div class="aesthetic-windows-95-button" style="margin: 10px auto">
+        <button>
+          OK
+        </button>
+      </div>
+    </div>
+  </div>
+`;
+// 个人卡片弹窗html
+const personal = `
+  <div class="aesthetic-windows-95-modal base-modal">
+    <div class="aesthetic-windows-95-modal-title-bar">
+      <div class="aesthetic-windows-95-modal-title-bar-text">
+        System
+      </div>
+      <div class="aesthetic-windows-95-modal-title-bar-controls">
+        <div class="aesthetic-windows-95-button-title-bar">
+          <button>
+            ×
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="aesthetic-windows-95-modal-content">
+      <div id="personalTab" class="aesthetic-windows-95-tabbed-container">
+        <div class="aesthetic-windows-95-tabbed-container-tabs">
+          <div class="aesthetic-windows-95-tabbed-container-tabs-button is-active">
+            <button onclick="tabClick(0)">
+              About Me
+            </button>
+          </div>
+          <div class="aesthetic-windows-95-tabbed-container-tabs-button">
+            <button onclick="tabClick(1)">
+              Motorcycle
+            </button>
+          </div>
+        </div>
+
+        <div id="personalContent" class="aesthetic-windows-95-container">
+          <!-- about me  -->
+          <div class="introduce-container is-active">
+            <div class="aesthetic-effect-crt">
+              <img src="/assets/image/me2.jpg" alt="">
+            </div>
+            <div class="introduce-content">
+              Hello everyone! I am Dingyi Zou, a frontend intern with 2 and a half years of work experience. I enjoy
+              music, gaming, and motorcycle. I am a very stubborn person, and I never care about what others say or
+              tell me to do. If you could be like me, I think that would be really cool!
+              <div style="margin-top: 50px">- thanks ❤</div>
+            </div>
+          </div>
+
+          <!-- motorcycle -->
+          <div class="motorcycle-container">
+            <img src="/assets/image/YAMAHA R3.png" alt="">
+            <p>
+              I am in possession of a YAMAHA R3 motorcycle, which brings me immense delight! Be it the mundane
+              task of daily commuting or the exhilarating weekend jaunts, this marvelous machine bestows upon me an
+              abundance of joy and invigoration!
+            </p>
+            <p>
+              If you, too, relish the thrill of traversing landscapes astride motorcycles, then our kinship is
+              forged!
+            </p>
+          </div>
+
+      </div>
+
+      <div class="aesthetic-windows-95-button" style="margin: 10px 0 0 auto">
+        <button>
+          OK
+        </button>
+      </div>
+    </div>
+  </div>
+`;
+
+// 创建弹窗方法
 function createWindows({
   top,
   left,
@@ -99,7 +196,6 @@ function createWindows({
     objectPool.shift();
   }
 }
-
 // 桌面图标事件
 function desktopIconClick(params) {
   if (params === "computer") {
@@ -126,121 +222,6 @@ function tabClick(index) {
   tabs[index].classList.add("is-active");
   personalContent.children[index].classList.add("is-active");
 }
-
-// 创建err弹窗
-const errHtml = `
-<div class="aesthetic-windows-95-modal">
-  <div class="aesthetic-windows-95-modal-title-bar">
-    <div class="aesthetic-windows-95-modal-title-bar-text">
-      Error
-    </div>
-
-    <div class="aesthetic-windows-95-modal-title-bar-controls">
-      <div class="aesthetic-windows-95-button-title-bar">
-        <button>×</button>
-      </div>
-    </div>
-  </div>
-  <!-- Content -->
-  <div class="aesthetic-windows-95-modal-content">
-    <div class="aesthetic-effect-text-glitch" data-glitch="Back to 90s ❤" style="margin: 7px auto">
-      Back to 90s ❤
-    </div>
-
-    <div class="aesthetic-windows-95-button" style="margin: 10px auto">
-      <button>
-        OK
-      </button>
-    </div>
-  </div>
-</div>`;
-for (let i = 0; i < 0; i++) {
-  setTimeout(
-    () =>
-      createWindows({
-        top: 100 + i * 15,
-        left: 200 + i * 15,
-        html: errHtml,
-      }),
-    i * 100
-  );
-}
-
-// 创建个人卡片弹窗
-const personal = `
-    <div class="aesthetic-windows-95-modal base-modal">
-      <div class="aesthetic-windows-95-modal-title-bar">
-        <div class="aesthetic-windows-95-modal-title-bar-text">
-          System
-        </div>
-        <div class="aesthetic-windows-95-modal-title-bar-controls">
-          <div class="aesthetic-windows-95-button-title-bar">
-            <button>
-              ×
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="aesthetic-windows-95-modal-content">
-        <div id="personalTab" class="aesthetic-windows-95-tabbed-container">
-          <div class="aesthetic-windows-95-tabbed-container-tabs">
-            <div class="aesthetic-windows-95-tabbed-container-tabs-button is-active">
-              <button onclick="tabClick(0)">
-                About Me
-              </button>
-            </div>
-            <div class="aesthetic-windows-95-tabbed-container-tabs-button">
-              <button onclick="tabClick(1)">
-                Motorcycle
-              </button>
-            </div>
-          </div>
-
-          <div id="personalContent" class="aesthetic-windows-95-container">
-            <!-- about me  -->
-            <div class="introduce-container ">
-              <div class="aesthetic-effect-crt">
-                <img src="/assets/image/me2.jpg" alt="">
-              </div>
-              <div class="introduce-content">
-                Hello everyone! I am Dingyi Zou, a frontend intern with 2 and a half years of work experience. I enjoy
-                music, gaming, and motorcycle. I am a very stubborn person, and I never care about what others say or
-                tell me to do. If you could be like me, I think that would be really cool!
-                <div style="margin-top: 50px">- thanks ❤</div>
-              </div>
-            </div>
-
-            <!-- motorcycle -->
-            <div class="motorcycle-container is-active">
-              <img src="/assets/image/YAMAHA R3.png" alt="">
-              <p>
-                I am in possession of a YAMAHA R3 motorcycle, which brings me immense delight! Be it the mundane
-                task of daily commuting or the exhilarating weekend jaunts, this marvelous machine bestows upon me an
-                abundance of joy and invigoration!
-              </p>
-              <p>
-                If you, too, relish the thrill of traversing landscapes astride motorcycles, then our kinship is
-                forged!
-              </p>
-            </div>
-
-        </div>
-
-        <div class="aesthetic-windows-95-button" style="margin: 10px 0 0 auto">
-          <button>
-            OK
-          </button>
-        </div>
-      </div>
-    </div>
-  `;
-createWindows({
-  top: 60,
-  left: 135,
-  width: 728,
-  height: 596,
-  html: personal,
-});
 
 // 创建gif popup
 function createGifPopup({ src, top, left, width, height }) {
@@ -271,14 +252,237 @@ function createGifPopup({ src, top, left, width, height }) {
   });
 }
 
+// 得到一个两数之间的随机整数，包括两个数在内
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值
+}
+
+// 随机定位图片
+const rangeT = 10;
+const rangeB = 800;
+const rangeL = 10;
+const rangeR = 1500;
 const gifItems = [
   {
-    src: "/assets/image/KIDMOGRAPH.gif",
-    top: 200,
-    left: 800,
-    width: 250 + 10, // 10 = padding: 8 + border: 2
-    height: 250 + 39, // 39 = padding-top: 5 + bar: 26 + padding: 8
+    src: "/assets/image/gif/vaporwave-1.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 500 + 10, // 10 = padding: 8 + border: 2
+    height: 375 + 39, // 39 = padding-top: 5 + bar: 26 + padding: 8
+  },
+  {
+    src: "/assets/image/gif/vaporwave-2.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 250 + 10,
+    height: 250 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-4.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 540 + 10,
+    height: 405 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-5.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 485 + 10,
+    height: 485 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-6.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 460 + 10,
+    height: 460 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-7.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 499 + 10,
+    height: 374 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-8.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 540 + 10,
+    height: 540 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-9.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 540 + 10,
+    height: 540 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-10.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 470 + 10,
+    height: 470 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-11.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 500 + 10,
+    height: 281 + 39,
+  },
+  {
+    src: "/assets/image/gif/vaporwave-12.gif",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 320 + 10,
+    height: 400 + 39,
   },
 ];
 
-gifItems.forEach((item) => createGifPopup(item));
+const imgItems = [
+  {
+    src: "/assets/image/citypop/1.png",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 400 + 10, // 10 = padding: 8 + border: 2
+    height: 400 + 39, // 39 = padding-top: 5 + bar: 26 + padding: 8
+  },
+  {
+    src: "/assets/image/citypop/2.jpg",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 400 + 10,
+    height: 400 + 39,
+  },
+  {
+    src: "/assets/image/citypop/3.jpg",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 380 + 10,
+    height: 380 + 39,
+  },
+  {
+    src: "/assets/image/citypop/5.jpg",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 500 + 10,
+    height: 500 + 39,
+  },
+  {
+    src: "/assets/image/citypop/6.png",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 380 + 10,
+    height: 380 + 39,
+  },
+  {
+    src: "/assets/image/citypop/7.png",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 380 + 10,
+    height: 380 + 39,
+  },
+  {
+    src: "/assets/image/citypop/8.jpg",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 350 + 10,
+    height: 350 + 39,
+  },
+  {
+    src: "/assets/image/citypop/9.jpg",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 400 + 10,
+    height: 400 + 39,
+  },
+  {
+    src: "/assets/image/citypop/10.jpg",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 450 + 10,
+    height: 450 + 39,
+  },
+  {
+    src: "/assets/image/citypop/11.png",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 500 + 10,
+    height: 499 + 39,
+  },
+  {
+    src: "/assets/image/citypop/12.png",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 350 + 10,
+    height: 279 + 39,
+  },
+  {
+    src: "/assets/image/citypop/13.png",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 300 + 10,
+    height: 300 + 39,
+  },
+  {
+    src: "/assets/image/citypop/14.jpg",
+    top: getRandom(rangeT, rangeB),
+    left: getRandom(rangeL, rangeR),
+    width: 400 + 10,
+    height: 380 + 39,
+  },
+];
+
+// 随机生成gif图片流
+function createPictures() {
+  const promises = [];
+  const randomImgs = gifItems
+    .concat(imgItems)
+    .filter(() => Math.random() > 0.3);
+
+  randomImgs.forEach((item, i) => {
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        createGifPopup(item);
+        resolve();
+      }, i * 100);
+    });
+    promises.push(promise);
+  });
+
+  return promises;
+}
+
+window.onload = (event) => {
+  // 创建个人卡片弹窗
+  createWindows({
+    top: 20,
+    left: 680,
+    width: 728,
+    height: 596,
+    html: personal,
+  });
+  const person = document.querySelector("#box1");
+  person.style.zIndex = 10;
+
+  // 图片流
+  Promise.all(createPictures()).then(() => {
+    // 创建err弹窗
+    for (let i = 0; i < 10; i++) {
+      setTimeout(
+        () =>
+          createWindows({
+            top: 50 + i * 15,
+            left: 100 + i * 15,
+            html: errHtml,
+          }),
+        i * 100
+      );
+    }
+  });
+};
