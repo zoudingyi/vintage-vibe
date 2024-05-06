04f11fd273ee4f50fd97aca7e9f41b001f87d77a
zoudingyi <zoudingyi@qq.com>
2024-05-06 16:23:24 +0800

    :construction: WIP(styles): update styles.

--- src/views/start-screen/index.jsx
+++ src/views/start-screen/index.jsx
@@ -1,5 +1,122 @@
+import './index.css';
+
 function StartScreen() {
-  return <div>StartScreen</div>;
+  const mountainStyles = [
+    {
+      '--mountain-base': '10vw',
+      '--mountain-height': '5vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#b533b3',
+      '--mountain-offset': '10vw',
+      '--mountain-tilt': '-20deg'
+    },
+    {
+      '--mountain-base': '10vw',
+      '--mountain-height': '5vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#681e6b',
+      '--mountain-tilt': '59deg',
+      '--mountain-offset': '20vw'
+    },
+    {
+      '--mountain-base': '8vw',
+      '--mountain-height': '4vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#b533b3',
+      '--mountain-offset': '20vw',
+      '--mountain-tilt': '-20deg'
+    },
+    {
+      '--mountain-base': '3vw',
+      '--mountain-height': '4vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#681e6b',
+      '--mountain-tilt': '45deg',
+      '--mountain-offset': '28vw'
+    },
+    {
+      '--mountain-base': '5vw',
+      '--mountain-height': '5vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#681e6b',
+      '--mountain-offset': '-40vw',
+      '--mountain-tilt': '-20deg'
+    },
+    {
+      '--mountain-base': '5vw',
+      '--mountain-height': '5vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#b533b3',
+      '--mountain-tilt': '33deg',
+      '--mountain-offset': '-35vw'
+    },
+    {
+      '--mountain-base': '5vw',
+      '--mountain-height': '5vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#681e6b',
+      '--mountain-tilt': '-62deg',
+      '--mountain-offset': '-5vw'
+    },
+    {
+      '--mountain-base': '10vw',
+      '--mountain-height': '5vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#2a025d',
+      '--mountain-tilt': '-20deg'
+    },
+    {
+      '--mountain-base': '10vw',
+      '--mountain-height': '5vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#150030',
+      '--mountain-tilt': '59deg',
+      '--mountain-offset': '10vw'
+    },
+    {
+      '--mountain-base': '10vw',
+      '--mountain-height': '10vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#150030',
+      '--mountain-tilt': '-33deg',
+      '--mountain-offset': '-30vw'
+    },
+    {
+      '--mountain-base': '10vw',
+      '--mountain-height': '10vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#2a025d',
+      '--mountain-offset': '-20vw',
+      '--mountain-tilt': '20deg'
+    },
+    {
+      '--mountain-base': '3vw',
+      '--mountain-height': '10vw',
+      '--mountain-color1': '#a684cb',
+      '--mountain-color2': '#681e6b',
+      '--mountain-tilt': '45.5deg',
+      '--mountain-offset': '-10vw'
+    }
+  ];
+
+  return (
+    <div className="start-screen">
+      <div className="site-heading">
+        <h1>vintage vibe</h1>
+      </div>
+      <div className="sun"></div>
+      <div className="entrances">Insert coin to continue...</div>
+      <div className="outrun-background-container">
+        <div className="background-80s stars">
+          <div className="grid"></div>
+          {mountainStyles.map(itemStyle => (
+            <div className="mountain" style={itemStyle}></div>
+          ))}
+          <div className="overlay"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StartScreen;
